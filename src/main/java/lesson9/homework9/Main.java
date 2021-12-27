b31c82063a5346664c61afc144cb34efa9c36c56
Kristina Petrova <trudarmne@gmail.com>
2021-12-27 22:20:46 +0300

    Homework9

---
+++ src/main/java/lesson9/homework9/Main.java
@@ -0,0 +1,41 @@
+package lesson9.homework9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    /*
+1. Написать функцию, принимающую список Student и возвращающую список уникальных курсов, на которые подписаны студенты.
+2. Написать функцию, принимающую на вход список Student и возвращающую список из трех самых любознательных
+(любознательность определяется количеством курсов).
+3. Написать функцию, принимающую на вход список Student и экземпляр Course, возвращающую список студентов, которые посещают этот курс.
+     */
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Olga", Arrays.asList(new Course("Biology"),
+                new Course("Testing"))));
+        students.add(new Student("Vlad", Arrays.asList(new Course("Math"), new Course("Biology"),
+                new Course("Testing"))));
+        students.add(new Student("Pavel", Arrays.asList(new Course("Math"),
+                new Course("Testing"), new Course("Physics"))));
+        students.add(new Student("Kristina", Arrays.asList(new Course("Physics"), new Course("Math"),
+                new Course("Biology"), new Course("Testing"), new Course("Geography"))));
+
+        System.out.println(students.stream()
+                .map(s -> s.getCourses())
+                .flatMap(f -> f.stream())
+                .collect(Collectors.toSet()));
+
+        System.out.println(students.stream()
+                .sorted((s1, s2) -> s2.getCourses().size() - s1.getCourses().size())
+                .limit(3)
+                .collect(Collectors.toList()));
+
+        Course course = new Course("Geography");
+        System.out.println(students.stream()
+                .filter(s -> s.getCourses().contains(course))
+                .collect(Collectors.toList()));
+    }
+}
